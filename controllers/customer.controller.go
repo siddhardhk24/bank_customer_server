147d1366e0a9f87fd8f34d4fcb5a552bdff1b6e2
siddhardhk24 <anandhsidhu541@gmail.com>
2023-08-31 12:07:44 +0530

    first commit

---
+++ controllers/customer.controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"context"
+	pro"github.com/grpc_bank/bank_customer_proto/netxd_customer"
+
+	"github.com/grpc_bank/bank_customer_service/interfaces"
+	"github.com/grpc_bank/bank_customer_service/models"
+)
+
+type RPCServer struct {
+	pro.UnimplementedCustomer_ServiceServer
+}
+
+var (
+	CustomerService interfaces.ICustomer
+)
+
+func (s *RPCServer) CreateCustomer(ctx context.Context, req *pro.Customer) (*pro.CustomerResponse, error) {
+	dbCustomer := &models.Customer{CustomerId: req.CustomerId}
+	result, err := CustomerService.CreateCustomer(dbCustomer)
+	if err != nil {
+		return nil, err
+	} else {
+		responseCustomer := &pro.CustomerResponse{
+			CustomerId: result.CustomerId,
+		}
+		return responseCustomer, nil
+	}
+}
---
+++ server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/grpc_bank/bank_customer_config/config"
+	"github.com/grpc_bank/bank_customer_config/constants"
+	pro "github.com/grpc_bank/bank_customer_proto/netxd_customer"
+	"github.com/grpc_bank/bank_customer_server/controllers"
+
+	"github.com/grpc_bank/bank_customer_service/services"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc"
+)
+
+func initDatabase(client *mongo.Client) {
+	profileCollection := config.GetCollection(client, "bankdb", "billa")
+	controllers.CustomerService = services.InitCustomerService(profileCollection, context.Background())
+}
+
+func main() {
+	mongoclient, err := config.ConnectDataBase()
+	defer mongoclient.Disconnect(context.TODO())
+	if err != nil {
+		panic(err)
+	}
+	initDatabase(mongoclient)
+	lis, err := net.Listen("tcp", constants.Port)
+	if err != nil {
+		fmt.Printf("Failed to listen: %v", err)
+		return
+	}
+	s := grpc.NewServer()
+	pro.RegisterCustomer_ServiceServer(s, &controllers.RPCServer{})
+
+	fmt.Println("Server listening on", constants.Port)
+	if err := s.Serve(lis); err != nil {
+		fmt.Printf("Failed to serve: %v", err)
+	}
+}
